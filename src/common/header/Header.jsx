9c83120f146a47325964e9a559bcdcdde05afff0
Vitaliy1804 <shervit82@gmail.com>
2022-11-08 19:45:37 +0200

    second commit

---
+++ src/common/header/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Header.css';
+import Navbar from './Navbar';
+import Search from './Search';
+import Head from './Head';
+
+const Header = () => {
+    return (
+    <>
+      <Head />  
+      <Search />   
+      <Navbar />
+    
+     
+    </>
+    
+    )
+}
+
+export default Header;
