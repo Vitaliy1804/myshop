808a2e4fddc5b3c26307a039355652461fc2c3a6
Vitaliy1804 <shervit82@gmail.com>
2022-11-12 21:06:40 +0200

    add slider

---
+++ src/components/mainpage/Slider.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import SlideCard from './SlideCard';
+
+const Slider = () => {
+    return (
+        <>
+       <section className="homeSlide contentwidth">
+        <div className="container">
+          <SlideCard />
+        </div>
+       </section>
+        </>
+    )
+}
+
+export default Slider;
