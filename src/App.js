2d6e66d874e4e83386b324707824883cbe8f6dc4
Vitaliy1804 <shervit82@gmail.com>
2022-11-15 21:20:09 +0200

    add flash slider

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import Header from './common/header/Header';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Pages from './pages/Pages';
+import Data from './components/flashDeals/Data';
+import { useState } from "react";
 
 function App() {
+  const { productItems } = Data;
+
+  const [cartItem, setCardItem] = useState([]);
   return (
     <>
     <Router>
       <Header />
       <Switch>
         <Route path='/' exact>
-          <Pages />
+          <Pages productItems={productItems} />
         </Route>
       </Switch>
     </Router>
