026b67ff7d611affe19e23ce3433924f205ee9a9
Vitaliy1804 <shervit82@gmail.com>
2022-12-06 11:22:08 +0200

    add new arrivals section

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import FlashDeals from '../components/flashDeals/flashDeals';
 import Home from '../components/mainpage/Home';
+import NewArrivals from '../components/mainpage/newarrivals/newArrivals';
+import TopCate from '../components/top/TopCate';
 
 const Pages = ({productItems, cartItem, addToCart}) => {
     return (
         <>
         <Home cartItem={cartItem} />
         <FlashDeals productItems={productItems} addToCart={addToCart} />
+        <TopCate />
+        <NewArrivals />
         </>
     )
 }
